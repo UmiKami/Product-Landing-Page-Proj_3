7422dfac8ee5502160f0934c4b0e0459bc08c1bf
UmiKami <ggoku02@gmail.com>
2021-01-13 19:17:30 -0500

    Major Responsiveness Issues Fixed, All Media Queries 70% done

--- app.js
+++ app.js
@@ -2,15 +2,36 @@ const viewVideoBtn = document.querySelector(".about-container__view-video-btn");
 const videoContainer = document.querySelector(".about-container__video-container");
 const closeVideoContainer = document.querySelector(".close-1");
 
+const cardContainer = document.querySelectorAll(".pricing-container__card-container");
+const cardList = document.querySelectorAll(".card-list");
 
 ////////////////////////////////////////////////////////////////////////////
 /// NAVBAR
 ////////////////////////////////////////////////////////////////////////////
 /// ABOUT SECTION
 viewVideoBtn.addEventListener(`click`, function(){
-    videoContainer.classList.add("slide-down")
+    videoContainer.classList.add("slide-down");
 })
 
 closeVideoContainer.addEventListener(`click`, function(){
-    videoContainer.classList.remove("slide-down")
+    videoContainer.classList.remove("slide-down");
+})
+
+// Iterate through the all the containers with the same class, aka, all the cards
+
+cardContainer.forEach(function(card){
+    card.addEventListener(`mouseenter`, function(eC){
+        cardList.forEach(function(eL){
+            if(eL.parentElement.parentElement.parentElement.parentElement.innerHTML == eC.currentTarget.innerHTML){
+                eL.classList.add("hide-card-text");
+            }
+        })
+    })
+    card.addEventListener(`mouseleave`, function(eC){
+        cardList.forEach(function(eL){
+            if(eL.parentElement.parentElement.parentElement.parentElement.innerHTML == eC.currentTarget.innerHTML){
+                eL.classList.remove("hide-card-text");
+            }
+        })
+    })
 })
